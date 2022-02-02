c61ea74c9239b6b3f3a8e582451a1bb967c03957
gayatridevik-mediwave <gayatridevi_k@mindwaveventures.com>
2022-02-02 19:09:44 +0530

    new edit

--- app.js
+++ app.js
@@ -7,26 +7,50 @@ const db = require("./db");
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    return res.send("hi");
+});
+
 //get all recipes
 app.get("/recipe", (req, res) => {
-    const recipe = db.addRecipe();
+    const recipe = db.recipe;
     return res.send(recipe);
 });
 
 //get one recipe
-app.get("./recipe:id", (req, res) => {
+app.get("/recipe/:id", (req, res) => {
     const recipeid = req.params.id;
-    const recipe = db.getOneRecipe(recipeid);
-    if (!addOnemovie) {
+    const getOneRecipe = db.getOneRecipe(recipeid);
+    if (!getOneRecipe) {
         return res.status(404).send({
             message: `recipe ${recipeid} not found`,
         });
     }
+    return res.send(getOneRecipe);
+});
+
+//add a recipe
+app.post("/recipe", (req, res) => {
+    const recipe = db.addRecipe(req.body);
     return res.send(recipe);
 })
 
-app.get("/", (req, res) => {
-    return res.send("hi");
+//delete a movie
+app.delete("/recipe/:id", (req, res) => {
+    const deleteId = req.params.id;
+    console.log(deleteId);
+    db.deleteRecipe(deleteId);
+    return res.send({
+        message: "Recipe deleted",
+    });
+});
+
+//update a movie
+app.put("/recipe/:id", (req, res) => {
+    const updateId = req.params.id;
+    const payload = req.body;
+    const recipe = db.updateRecipe(updateId, payload);
+    return res.send(recipe);
 });
 
 app.listen(port, (err) => {
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const recipe = [
     {
-        id: "101",
+        id: 101,
         recpname: "chicken briyani",
         foodType: "non-veg",
         ingredients: "hi",
@@ -11,7 +11,7 @@ const recipe = [
         },
     },
     {
-        id: "102",
+        id: 100,
         recpname: "Pasta",
         foodType: "veg",
         ingredients: "hi",
@@ -22,7 +22,7 @@ const recipe = [
         },
     },
     {
-        id: "103",
+        id: 102,
         recpname: "Vegtable Soup",
         foodType: "veg",
         ingredients: "hi",
@@ -34,31 +34,43 @@ const recipe = [
     }
 ];
 
+const getOneRecipe = (recipeid) => {
+    const getOneRecipe = recipe.find((m) =>
+        m.id == recipeid);
+    return getOneRecipe;
+};
+
 const addRecipe = (payload) => {
-    const add = {
-        // id: new Date().getTime(),
+    const recipea = {
+        id: new Date().getTime(),
         ...payload,
-        // recpname : payload.recpname,
-        // foodType : payload.foodType,
-        // ingredients : payload.ingredients,
-        // stepstoPrepare : payload.stepstoPrepare,
-        // images: {
-        //     payload.url,
-        //     payload.altInfo,
-        // },
+    };
+    recipe.push(recipea);
+    return recipea;
+};
+
+const deleteRecipe = (deleteId) => {
+    const deletedIndex = recipe.findIndex((m) => m.id == deleteId);
+    if (deletedIndex == -1) {
+        console.log("movie not found");
     }
-    recipe.push(add);
-    return recipe;
+    recipe.splice(deletedIndex, 1);
 }
 
-const addOneRecipe = (payload) => {
-    const addOne = {
-        ...payload,
-    }
+const updateRecipe = (updateId, payload) => {
+    const updateIndex = recipe.findIndex((m) => m.id == updateId);
+    if (updateIndex == -1) {
+        console.log("recipe not found");
+    } recipe[updateIndex]["recpname"] = payload.recpname;
+
+    return recipe[updateIndex];
+    // movies[index]["name"] = payload.name;
 }
 
 module.exports = {
     recipe,
+    getOneRecipe,
     addRecipe,
-    addOneRecipe,
+    deleteRecipe,
+    updateRecipe,
 }
